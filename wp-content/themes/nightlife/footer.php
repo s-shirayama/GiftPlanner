ca27c3a34210a0f59e4bf305f6f3fe67c55d705d
affiliate <shirayama5288@gmail.com>
2013-05-19 22:28:04 +0900

    TS:フッター修正

--- wp-content/themes/nightlife/footer.php
+++ wp-content/themes/nightlife/footer.php
@@ -3,24 +3,27 @@
 <div style="clear:both;"></div>
 <div style="clear:both;"></div>
 
- <div id="gift_infobar">
+<div id="gift_infobar">
   <a href="<?php echo get_settings('home'); ?>/description">GiftPlanner でできること</a>
   &nbsp;&nbsp;|&nbsp;&nbsp;
+  <a href="<?php echo get_settings('home'); ?>/member">サイト運営者紹介</a>
+  &nbsp;&nbsp;|&nbsp;&nbsp;
+  <a href="<?php echo get_settings('home'); ?>/category/開発者ブログ">開発者ブログ</a>
+  &nbsp;&nbsp;|&nbsp;&nbsp;
   <a href="<?php echo get_settings('home'); ?>/sitemap">サイトマップ</a>
- </div>
+</div>
 
- <div id="gift_style2">
+<div id="gift_style2">
   【免責事項】<br />
   本サイトは個人名から極力最適なプレゼント・商品を検索するサービスです。<br/>
   当サイトの御利用につき、何らかのトラブル等が発生した場合でも一切責任を問わないものとします。
- </div>
+</div>
 
 <div id="footer">
-Copyright &copy; <a href="<?php echo get_settings('home'); ?>/"><?php bloginfo('name'); ?></a>
-<!--&bull; Nightlife theme by <a href="http://wpthemejp.com/">WordPress theme</a>-->
+  Copyright &copy; <a href="<?php echo get_settings('home'); ?>/"><?php bloginfo('name'); ?></a>
+  <!--&bull; Nightlife theme by <a href="http://wpthemejp.com/">WordPress theme</a>-->
 </div>
 
 <?php do_action('wp_footer'); ?>
 
 </body>
-</html>
