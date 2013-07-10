eb74ef4da791bba59ae1568f301973e8fe2ec90e
affiliate <shirayama5288@gmail.com>
2013-07-10 23:33:26 +0900

    All Release

---
+++ wp-content/themes/nightlife/ranking.php
@@ -0,0 +1,72 @@
+<?php
+$url = preg_split('/\//',$_SERVER['REQUEST_URI']);
+$responses = get_ranking_info_from_key( $url[2], 10 );
+$response = $responses[0]['items'];
+?>
+
+<?php get_header(); ?>
+<div id="content_gp_middle">
+  <div id="content_gp_top">
+    <div id="content"> 
+      <div class="gp_social_wg"><?php echo get_the_social_widgets();?></div>
+ 
+	  <div class="gp_top_main">
+
+	  <?php $loop_no = 1; ?>
+      <?php foreach( $responses as $response): ?>
+
+    <script type="text/javascript">
+    $(document).ready(function(){
+        $("#my-als-list-<?php echo $loop_no; ?>").als({
+                    visible_items: 5,
+                    scrolling_items: 3,
+                    orientation: "horizontal",
+                    circular: "no",
+                    autoscroll: "no",
+        });
+    });
+    </script>
+
+      <div class="gp_title_page"><h2><?php echo $response['genre']; ?></h2></div>
+
+	<div id="lista1">
+	  <div class="als-container" id="my-als-list-<?php echo $loop_no; ?>">
+	    <span class="als-prev"><img src="<?php echo get_settings('home'); ?>/wp-content/themes/nightlife/images_gift/gp_thin_left_arrow.png" alt="prev" title="previous" /></span>
+
+	    <div class="als-viewport">
+	      <?php $items = $response['items']; ?>
+	      <ul class="als-wrapper">
+
+		<?php foreach ($items as $item_tmp): ?>
+		<?php $item = $item_tmp['Item']; ?>
+		<?php if (!empty($item['smallImageUrls'][0]['imageUrl'])): ?>
+		<li class="als-item">
+		  <div id="lista2">
+		    <a href="<?php echo h($item['affiliateUrl']) ?>" class="itemname" title="<?php echo h($item['itemName']) ?>" target="_blank"><img src="<?php echo h($item['mediumImageUrls'][0]['imageUrl']) ?>"></a>
+		    <?php endif; ?>
+		    <?php echo '<div class="starlevel5 star' . sprintf('%02d',floor($item['reviewAverage']*2)*5) . '"></div>' ?>
+		    <!--<li class="addbookmark"><a href="bookmark.php?itemCode=<?php echo h($item['itemCode']) ?>&amp;keyword=<?php echo h($keyword) ?>&amp;page=<?php echo h($page) ?>">ブックマークへ追加</a></li>-->
+		    <a href="<?php echo h($item['affiliateUrl']) ?>" class="itemname" title="<?php echo h($item['itemName']) ?>" target="_blank"><?php echo h(mb_strimwidth($item['itemName'], 0, 30, '...', 'UTF-8')) ?></a>
+		    <p><?php echo h(number_format($item['itemPrice'])) ?>円</p>
+		    <p>[ <a href="<?php echo h($item['affiliateUrl']) ?>" class="itemname" target="_blank">詳細はこちら</a> ]</p>
+			<br/><br/>
+		  </div>
+	    </li>
+		<?php endforeach; ?>
+
+	      </ul>
+	      <!--<hr size="0.7px" noshade />-->
+	    </div>
+
+	    <span class="als-next"><img src="<?php echo get_settings('home'); ?>/wp-content/themes/nightlife/images_gift/gp_thin_right_arrow.png" alt="next" title="next" /></span><!-- "next" button -->
+	  </div><!-- my-als-list -->
+	</div><!-- lista1 -->
+
+	  <?php $loop_no = $loop_no + 1;?>
+	  <?php endforeach; ?>
+
+	  </div><!-- gp_top_main -->
+    </div><!-- content -->
+  </div><!-- content_gp_top -->
+</div><!-- content_gp_middle -->
+<?php get_footer(); ?>
---
+++ wp-content/themes/nightlife/sidemenu.php
@@ -0,0 +1,36 @@
+<div id="sideNavigation">
+  <div id="sideMenu">メニュー</div>
+  <div class="inner">
+    <span class="title02">あげたい人で絞り込む</span>
+    <ul class="arrow">
+      <li><a href="/ranking/boyfriend/">彼氏</a></li>
+      <li><a href="/ranking/girlfriend/">彼女</a></li>
+      <li><a href="/ranking/husband/">ご主人・夫</a></li>
+      <li><a href="/ranking/wife/">奥さん ・妻</a></li>
+      <li><a href="/ranking/father/">お父さん・父親 </a></li>
+      <li><a href="/ranking/mother/">お母さん・母親 </a></li>
+      <li><a href="/ranking/grandfather/">おじいちゃん・祖父</a></li>
+      <li><a href="/ranking/grandmother/">おばあちゃん・祖母</a></li>
+      <li><a href="/ranking/baby/">赤ちゃん</a></li>
+      <li><a href="/ranking/pregnant/">妊婦</a></li>
+      <p class="right li2"><a href="/ranking/target_list">あげたい人一覧を見る</a></p>
+    </ul>
+    <span class="title02">イベントで絞り込む</span>
+    <ul class="arrow">
+      <li><a href="/ranking/birthday/">誕生日</a></li>
+      <li><a href="/ranking/wedding/">結婚祝い</a></li>
+      <li><a href="/ranking/babygifts/">出産祝い </a></li>
+      <li><a href="/ranking/birthfamilycelebration/">出産内祝い </a></li>
+      <li><a href="/ranking/anniversary/">記念日</a></li>
+      <li><a href="/ranking/weddinganniversary/">結婚記念日</a></li>
+      <li><a href="/ranking/housewarming/">新築祝い </a></li>
+      <li><a href="/ranking/housewarm/">引っ越し祝い</a></li>
+      <li><a href="/ranking/retirement/">退職祝い</a></li>
+      <li><a href="/ranking/celebratesixty/">還暦祝い </a></li>
+      <p class="right li2"><a href="/ranking/event_list">イベント一覧を見る</a></p>
+    </ul>
+    <ul class="arrow end">
+      <li><a href="/description">GiftPlannerとは</a></li>
+    </ul>
+  </div>
+</div>
