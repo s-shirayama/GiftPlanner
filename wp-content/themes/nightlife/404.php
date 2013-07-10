f4cc084be5913837a08b7a3a2c67071b03abea78
affiliate <shirayama5288@gmail.com>
2013-07-11 02:49:15 +0900

    TS:エラーページ

--- wp-content/themes/nightlife/404.php
+++ wp-content/themes/nightlife/404.php
@@ -1,18 +1,22 @@
 <?php get_header(); ?>
-
+<div id="content_gp_middle">
+<div id="content_gp_top">
 <div id="content">
 
-<?php include(TEMPLATEPATH."/l_sidebar.php");?>
+<?php /* include(TEMPLATEPATH."/l_sidebar.php"); */ ?>
 
-<div id="contentmiddle">
-
-<div class="contenttitle">
-	<h1>お探しのページは見つかりませんでした。</h1><br />
-	<p>他のキーワードで検索してください。</p>
-	</div>
-	
+<div class="gp_title_page">
+	<h2>お探しのページは見つかりませんでした。</h2>
 </div>
+<div class="entry-content">
+	<p>他のキーワードで検索してください。</p>
+</div>
+	
 
 <!-- The main column ends  -->
 
+</div>
+</div>
+</div>
+</div><!-- wrap -->
 <?php get_footer(); ?>
